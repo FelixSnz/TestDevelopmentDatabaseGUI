40d96df6f4ec17e0cadf41fd67455ef4e49cd31b
FelixSnz <felixsanchez0124@gmail.com>
2023-06-27 11:32:52 -0500

    deleted unecesary files

--- Scripts/testreadfile.py
+++
@@ -1,9 +0,0 @@
-
-
-path = r"\\c566s003\Data\Shared\BasedeDatos_Instrumentacion\Sequencias\test.txt"
-
-with open(path, 'r') as f:
-    # Read the contents of the file
-    contents = f.read()
-    # Print the contents of the file
-    print(contents)
